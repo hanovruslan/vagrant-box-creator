9d81a645cfcb34362d0f164f97bae3f418ebbf0b
Hanov Ruslan <evolaze@yandex-team.ru>
2016-12-10 14:28:57 +0300

    Added vagrant pack helper

---
+++ Vagrantfile
@@ -0,0 +1,9 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+$name = ENV.key?("name") ? ENV["name"] : "default"
+file = ENV.key?("file") ? ENV["file"] : $name + ".vagrantfile"
+path = ENV.key?("path") ? ENV["path"] : File.dirname(__FILE__)
+
+load File.expand_path(file, path)
+load File.expand_path("pack-post-action.vagrantfile", File.dirname(__FILE__))
---
+++ pack.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+name=${1:-default}
+path="${2:-./}"
+file=${3:-${name}.vagrantfile}
+
+export name=${name} \
+&& export path=${path} \
+&& export file=${file} \
+&& vagrant destroy -f \
+&& vagrant up --provision \
+&& vagrant box update \
+&& rm -f ./boxes/${name}.box \
+&& realpath .vagrant/machines/${name}/virtualbox/id 1>/dev/null 2 >&1 \
+&& vagrant package \
+  --base $(cat .vagrant/machines/${name}/virtualbox/id) \
+  --output ./boxes/${name}.box \
+&& vagrant box add --force ${name} ./boxes/${name}.box \
+&& rm -f ./boxes/${name}.box \
+&& vagrant destroy -f
