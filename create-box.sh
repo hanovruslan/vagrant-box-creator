3e9abba6c5a7bd5fa3d09faadd7258bdc010d0c8
hanovruslan <hanov.ruslan@gmail.com>
2017-04-23 06:39:16 +0300

    Added simple examples

--- create-box.sh
+++ create-box.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-name=${1:-default}
-path="${2:-./}"
-file=${3:-${name}.vagrantfile}
+name=${1:-example}
+path="${2:-./examples}"
+file=${3:-${name}-box.vagrantfile}
+box_path=${4:-./boxes}
 provider=virtualbox
 
 export name=${name} \
@@ -11,11 +12,11 @@ export name=${name} \
 && vagrant destroy -f \
 && vagrant up --provision \
 && vagrant box update \
-&& rm -f ./boxes/${name}.box \
+&& rm -f ${box_path}/${name}.box \
 && realpath .vagrant/machines/${name}/${provider}/id 1>/dev/null 2 >&1 \
 && vagrant package \
   --base $(cat .vagrant/machines/${name}/${provider}/id) \
-  --output ./boxes/${name}.box \
-&& vagrant box add --force ${name} ./boxes/${name}.box \
-&& rm -f ./boxes/${name}.box \
+  --output ${box_path}/${name}.box \
+&& vagrant box add --force ${name} ${box_path}/${name}.box \
+&& rm -f ${box_path}/${name}.box \
 && vagrant destroy -f
